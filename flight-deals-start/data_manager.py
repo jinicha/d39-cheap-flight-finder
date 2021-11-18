f6fd1c12881b818a9e34236debaffe29551c85e2
jinicha <jinichaa@gmail.com>
2021-11-17 21:41:20 -0800

    Get data from google sheet and store it

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,3 +1,21 @@
+import requests
+from pprint import pprint
+import config
+
+TOKEN = config.TOKEN
+
+sheety_url = "https://api.sheety.co/1535b87ed86e1f83ec9c3d5640e64173/flightDeals/prices"
+sheety_header = {
+    "Authorization": f'Bearer {TOKEN}',
+    "Content-type": "application/json"
+}
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.sheety_data = {}
+
+    def get_sheety_data(self):
+        response = requests.get(url=sheety_url, headers=sheety_header)
+        self.sheety_data = response.json()["prices"]
+        return self.sheety_data
